e40cda059216474368bcb1b1292ab7afaedf1898
Daniel S <danielstawiarski@wp.pl>
2023-04-07 07:39:01 +0200

    Reformat calculation service

--- src/test/java/pl/danielstawiarski/githubtask/service/CalculationServiceTest.java
+++ src/test/java/pl/danielstawiarski/githubtask/service/CalculationServiceTest.java
@@ -9,19 +9,16 @@ public class CalculationServiceTest {
 
     @Test
     void shouldCalculateForSmallNumbers() {
-        Assertions.assertEquals(2.0, calculationService.calculate(6, 1),
-                0);
+        Assertions.assertEquals(2.0, calculationService.calculate(6, 1), 0);
     }
 
     @Test
     void shouldReturnZeroWhenUserDoesNotHaveFollowers() {
-        Assertions.assertEquals(0, calculationService.calculate(0, 100),
-                0);
+        Assertions.assertEquals(0, calculationService.calculate(0, 100), 0);
     }
 
     @Test
     void shouldCalculateForResultWithManyNumbersAfterComma() {
-        Assertions.assertEquals(0.45569620253164556, calculationService.calculate(79, 3),
-                0);
+        Assertions.assertEquals(0.45569620253164556, calculationService.calculate(79, 3), 0);
     }
 }
